eab155a027581f4d58ade92111f333784858bc37
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-09-28 15:01:57 -0300

    Feat: Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,30 @@
+def content_with_word(word, instance, callback):
+
+    response = []
+
+    for item, process in enumerate(instance.queue):
+        ocorrencias = []
+
+        for line, l in enumerate(process['linhas_do_arquivo']):
+
+            if word.lower() in l.lower():
+                ocorrencias.append(callback(line, l))
+
+        if len(ocorrencias) > 0:
+            response.append({
+                "palavra": word,
+                "arquivo": instance.queue[item]['nome_do_arquivo'],
+                "ocorrencias": ocorrencias
+            })
+
+    return response
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    def callback(line, _):
+        return {"linha": line + 1}
+
+    return content_with_word(word=word, instance=instance, callback=callback)
 
 
 def search_by_word(word, instance):
